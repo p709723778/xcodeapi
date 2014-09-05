779bf2f91a0941bfc333d8dfddef6b24d5ef967c
Povilas Kanapickas <povilas@unity3d.com>
2014-09-05 10:45:29 +0300

    Remove extra references to UnityEngine

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using UnityEngine;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
